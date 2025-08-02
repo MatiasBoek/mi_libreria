5d1c71e10d5ab575388ebdc247fcafafb6113387
MatiasBoek <matias.boekenhans@gmail.com>
2025-08-02 03:33:48 -0300

    Decimo Septimo Commit. Se modificaron todos los estilos de los html y se hicieron cambios para que funcione el menu desplegable del ususuario en todos los html; además, se modificó el estilo de base.html y se migró a todos los demas

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Autor, Editorial, Libro, Resena, Page 
 
-# Register your models here.
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'editorial', 'año_publicacion', 'precio') 
+    list_filter = ('autor', 'editorial')  
+    search_fields = ('titulo', 'autor__nombre')  
+    ordering = ('titulo',)  
+
+admin.site.register(Autor)
+admin.site.register(Editorial)
+admin.site.register(Resena)
+admin.site.register(Page)
+
